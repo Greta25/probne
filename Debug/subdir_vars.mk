179d37c4ff2e582c3d3fdbfa00047f560bf861ce
greta25 <gretamiotla@gmail.com>
2020-04-21 21:28:20 +0200

    Dodanie wszystkich plikow

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr6989.cmd 
+
+C_SRCS += \
+../hello.c 
+
+OBJS += \
+./hello.obj 
+
+C_DEPS += \
+./hello.d 
+
+C_DEPS__QUOTED += \
+"hello.d" 
+
+OBJS__QUOTED += \
+"hello.obj" 
+
+C_SRCS__QUOTED += \
+"../hello.c" 
+
+
